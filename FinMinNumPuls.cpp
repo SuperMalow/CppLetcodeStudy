cc2f37b0470595bb9d51c871974104f74c6f5a7c
Supermalow <229538252@qq.com>
2021-04-18 12:19:26 +0800

    master "优化版"

---
+++ FinMinNumPuls.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+private:
+    vector <int> quickSort(vector<int> &arr,int k,int l,int r)
+    {
+        int i = l,j = r;
+        while (i < j)
+        {
+            while (i < j && arr[j] >= arr[l])
+            {
+                j--;
+            }
+            while (i < j && arr[i] <= arr[l])
+            {
+                i++;
+            }
+            swap(arr[i],arr[j]);
+
+        }
+        swap(arr[i],arr[l]);
+        if  (i > k)
+        {
+            return quickSort(arr,k,l,i-1);
+        }
+        if  (i < k)
+        {
+            return quickSort(arr,k,i+1,r);
+        }
+    vector <int> res;
+    res.assign(arr.begin(),arr.begin()+k);
+    return res;
+
+    }
+public:
+    vector <int> getListNumbers(vector<int> & arr,int k)
+    {
+        if (k >= arr.size()) return arr;
+        return quickSort(arr,k,0,arr.size()-1);
+    }
+};
+
+int main()
+{
+    system("pause");
+    return 0;
+}
